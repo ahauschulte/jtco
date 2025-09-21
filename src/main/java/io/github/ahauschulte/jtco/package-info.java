8da4ff0ce2dbd0c13bc89d29c58f1b14092db8b7
Axel Hauschulte <axel.hauschulte@gmail.com>
2025-09-21 15:36:59 +0200

    Refine JavDoc in package-info

--- src/main/java/io/github/ahauschulte/jtco/package-info.java
+++ src/main/java/io/github/ahauschulte/jtco/package-info.java
@@ -1,11 +1,12 @@
 /**
  * Contains types for the optimised execution of tail calls.
  *
- * <p>This package provides a means for implementing tail call optimisation in Java, allowing
- * recursive algorithms to be executed in an iterative manner, thus avoiding deep call stacks and improving the
- * performance and reliability of recursive methods.
+ * <p>This package provides a substitute for native tail-call optimisation in Java, allowing
+ * tail-recursive algorithms to be executed iteratively in constant stack space, thereby preventing deep call stacks
+ * and stack overflows while improving robustness. Performance characteristics depend on the workload and are typically
+ * slower than equivalent handwritten iterative implementations due to additional indirection and allocation overhead.
  *
- * <p>The main component of this package is {@link io.github.ahauschulte.jtco.TailCall}: An interface representing a
+ * <p>The main component of this package is {@link io.github.ahauschulte.jtco.TailCall}, an interface representing a
  * tail call that can be evaluated iteratively to avoid deep recursion.
  */
 package io.github.ahauschulte.jtco;
